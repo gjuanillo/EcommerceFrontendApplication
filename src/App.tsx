5c5ca665d72a79eb10b56c0fa02d48d00a350a15
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-15 13:37:26 +0800

    Create Product Component Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { FaBeer } from 'react-icons/fa'
 import './App.css'
+import Products from './components/Products'
 
 function App() {
 
     return (
-        <div className="flex justify-center items-center w-screen min-h-screen bg-gray-900">
-            <h2 className="text-2xl font-extrabold text-center text-teal-300">Initialize Project <FaBeer /> </h2>
-        </div>
+        <>
+            <Products />
+        </>
     )
 }
 
