6e4924dc867e9bfb293024c94a640ea246f357e0
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-16 16:17:27 +0800

    Fetch Products from Backend App

--- src/store/reducers/store.ts
+++ src/store/reducers/store.ts
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { ProductReducer } from './ProductReducer';
 
 const store = configureStore({
-    reducer: {},
-    preloadedState: {}
+    reducer: {
+        products: ProductReducer
+    },
+    preloadedState: {
+    }
 })
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
