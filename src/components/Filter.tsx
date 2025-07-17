d82494d9ae49c8ef299812d35c2a5badc080e9c6
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-17 14:33:13 +0800

    Manipulate URL using Search Terms & Filters

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,7 @@
 import { FormControl, IconButton, InputLabel, MenuItem, Select, Tooltip, type SelectChangeEvent } from "@mui/material";
-import { useState } from "react";
-import { FaSearch } from "react-icons/fa";
-import { RiFilterOffLine, RiSortAsc } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import { RiFilterOffLine, RiSearch2Line, RiSortAsc, RiSortDesc } from "react-icons/ri";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 type Category = {
     categoryId: number,
@@ -19,17 +19,69 @@ const Filter = () => {
         { categoryId: 7, categoryName: "Power Supply Unit (PSU)" },
         { categoryId: 8, categoryName: "PC Case" },
     ];
+    const [searchParams] = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+    const pathName = useLocation().pathname;
+    const navigate = useNavigate();
+
     const [category, setCategory] = useState<string>("all");
+    const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    useEffect(() => {
+        const currentCategory = searchParams.get("category") || "all";
+        const currentSortOrder = (searchParams.get("sortby") as "asc" | "desc") || "asc";
+        const currentSearchTerm = searchParams.get("keyword") || "";
+        setCategory(currentCategory);
+        setSortOrder(currentSortOrder);
+        setSearchTerm(currentSearchTerm);
+    }, [searchParams]);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            if (searchTerm) {
+                searchParams.set("keyword", searchTerm);
+            } else {
+                searchParams.delete("keyword");
+            }
+            navigate(`${pathName}?${searchParams.toString()}`)
+        }, 600);
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [searchParams, searchTerm, navigate, pathName]);
+
     const handleCategoryChange = (event: SelectChangeEvent): void => {
-        setCategory(event.target.value)
+        const selectedCategory = event.target.value;
+        if (selectedCategory === "all") {
+            params.delete("category");
+        } else {
+            params.set("category", selectedCategory);
+        }
+        navigate(`${pathName}?${params}`)
+        setCategory(selectedCategory);
     };
+
+    const handleClearFilters = (): void => {
+        navigate({ pathname: window.location.pathname });
+    }
+
+    const toggleSortOrder = (): void => {
+        setSortOrder((prev) => {
+            const newOrder: "asc" | "desc" = prev === "asc" ? "desc" : "asc";
+            params.set("sortby", newOrder);
+            navigate(`${pathName}?${params}`)
+            return newOrder
+        });
+    }
+
     return (
         <div className="flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4">
             {/* Search Bar */}
             <div className="relative flex items-center 2xl:w-[450px] sm:w-[420px] w-full">
-                <input type="text" placeholder="Looking for something?"
+                <input type="text" placeholder="Looking for something?" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
                     className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-400" />
-                <FaSearch className="absolute left-3 text-slate-800 size={20}" />
+                <RiSearch2Line className="absolute left-3 text-slate-800 size={20}" />
             </div>
 
             {/* Category Selector */}
@@ -48,13 +100,15 @@ const Filter = () => {
 
                 {/* Sort & Filter */}
                 <Tooltip title="Sorted by price: asc">
-                    <IconButton size="small"
+                    <IconButton size="small" onClick={toggleSortOrder}
                         className="flex items-center gap-2 h-10">
-                        <RiSortAsc />
+                        {sortOrder === "asc" ?
+                            <RiSortAsc /> : <RiSortDesc />
+                        }
                     </IconButton>
                 </Tooltip>
                 <Tooltip title="Clear Filter">
-                    <IconButton size="small"
+                    <IconButton size="small" onClick={handleClearFilters}
                         className="flex items-center gap-2 h-10">
                         <RiFilterOffLine />
                     </IconButton>
