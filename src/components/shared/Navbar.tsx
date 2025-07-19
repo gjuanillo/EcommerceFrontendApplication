0353e9cb8faf71e965997fa1ab3fcd30857c5826
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-19 20:37:11 +0800

    Add Responsive Hamburger

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,12 +2,13 @@ import { Badge } from "@mui/material";
 import { useState } from "react";
 import { FaShoppingCart, FaSignInAlt, FaStore } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
+import { Spiral as Hamburger } from 'hamburger-react'
 
 const Navbar = () => {
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-        <div className="h-[50px] bg-teal-800 text-white z-50 flex items-center sticky top-0">
+        <div className="sm:h-[50px] h-[70px] bg-teal-800 text-white z-50 flex items-center sticky top-0">
             <div className="lg:px-14 sm:px-8 px-4 w-full flex justify-between">
                 <Link to="/" className="flex items-center text-2xl font-bold">
                     <FaStore className="mr-2 text-3xl" />
@@ -54,13 +55,16 @@ const Navbar = () => {
                         </Link>
                     </li>
                     <li className="font-[500] transition-all duration-150">
-                        <Link className="flex items-center space-x-2 px-4 border rounded-xl"
+                        <Link className="flex justify-center items-center space-x-2 px-4 border rounded-xl hover:text-slate-400"
                             to="/login">
                             <FaSignInAlt />
                             <span>Login</span>
                         </Link>
                     </li>
                 </ul>
+                <div className="sm:hidden flex items-center sm:mt-0 mt-2">
+                    <Hamburger toggled={navbarOpen} toggle={setNavbarOpen} size={20}/>
+                </div>
             </div>
         </div>
     )
